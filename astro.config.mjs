f7087ba4b813a2fad9997a67051b5d1f4f2f8a0f
Maxiim3 <maximemickael.contact@gmail.com>
2025-05-17 13:44:06 +0200

    chore - Add vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), react({
+    experimentalReactChildren: true,
+  }), vue()],
 });
