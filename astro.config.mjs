42ec6f0bbeb0f9af88424d3e2b74548771c494a9
Maxiim3 <maximemickael.contact@gmail.com>
2025-05-18 10:38:11 +0200

    depedency: add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,24 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
-
 import vue from "@astrojs/vue";
-
 import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), react({
-    experimentalReactChildren: true,
-  }), vue(), svelte()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react({
+      experimentalReactChildren: true,
+    }),
+    vue(),
+    svelte(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
